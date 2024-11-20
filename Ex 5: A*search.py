0af1678b7ea885571bde875f7610be573908f16f
ShiivRS331 <220701331@rajalakshmi.edu.in>
2024-11-20 14:30:57 +0530

    Update Ex 5: A*search.py ok

--- Ex 5: A*search.py
+++ Ex 5: A*search.py
@@ -1,73 +1,74 @@
-class Graph:
-    def __init__(self, adjac_lis):
-        self.adjac_lis = adjac_lis
+def get_neighbors(v, adjac_lis):
+    return adjac_lis[v]
 
-    def get_neighbors(self, v):
-        return self.adjac_lis[v]
+def h(n):
+    # Heuristic function (estimate of cost from n to goal)
+    H = {
+        'A': 4,  # Assuming heuristic values based on distance to 'D'
+        'B': 3,
+        'C': 2,
+        'D': 0  # Goal node heuristic is 0
+    }
+    return H[n]
 
-    def h(self, n):
-        H = {
-            'A': 1,
-            'B': 1,
-            'C': 1,
-            'D': 1
-        }
-        return H[n]
+def a_star_algorithm(start, stop, adjac_lis):
+    open_lst = set([start])  # Nodes to be evaluated
+    closed_lst = set([])     # Nodes already evaluated
+    poo = {}                 # g(n): cost from start to node
+    poo[start] = 0
+    par = {}                 # Parent of each node for path reconstruction
+    par[start] = start
 
-    def a_star_algorithm(self, start, stop):
-        open_lst = set([start])
-        closed_lst = set([])
-        poo = {}
-        poo[start] = 0
-        par = {}
-        par[start] = start
+    while len(open_lst) > 0:
+        n = None
+        # Select the node with the lowest f = g + h
+        for v in open_lst:
+            if n is None or poo[v] + h(v) < poo[n] + h(n):
+                n = v
 
-        while len(open_lst) > 0:
-            n = None
-            for v in open_lst:
-                if n == None or poo[v] + self.h(v) < poo[n] + self.h(n):
-                    n = v
+        if n is None:
+            print('Path does not exist!')
+            return None
 
-            if n == None:
-                print('Path does not exist!')
-                return None
+        if n == stop:
+            reconst_path = []
+            while par[n] != n:
+                reconst_path.append(n)
+                n = par[n]
+            reconst_path.append(start)  # Add start node
+            reconst_path.reverse()
 
-            if n == stop:
-                reconst_path = []
-                while par[n] != n:
-                    reconst_path.append(n)
-                    n = par[n]
-                reconst_path.append(start)
-                reconst_path.reverse()
+            print('Path found: {}'.format(reconst_path))
+            return reconst_path
 
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
-
-            for (m, weight) in self.get_neighbors(n):
-                # if the current node is not present in both open_lst and closed_lst
-                if m not in open_lst and m not in closed_lst:
-                    open_lst.add(m)
-                    par[m] = n
+        # Update costs and parents for neighbors
+        for (m, weight) in get_neighbors(n, adjac_lis):
+            if m not in open_lst and m not in closed_lst:
+                open_lst.add(m)
+                par[m] = n
+                poo[m] = poo[n] + weight
+            else:
+                if poo[m] > poo[n] + weight:
                     poo[m] = poo[n] + weight
-                else:
-                    if poo[m] > poo[n] + weight:
-                        poo[m] = poo[n] + weight
-                        par[m] = n
+                    par[m] = n
+                    if m in closed_lst:
+                        closed_lst.remove(m)
+                        open_lst.add(m)
 
-                        if m in closed_lst:
-                            closed_lst.remove(m)
-                            open_lst.add(m)
+        open_lst.remove(n)
+        closed_lst.add(n)
 
-            open_lst.remove(n)
-            closed_lst.add(n)
+    print('Path does not exist!')
+    return None
 
-        print('Path does not exist!')
-        return None
 
+# Example Graph with adjacency list format
 adjac_lis = {
     'A': [('B', 1), ('C', 3), ('D', 7)],
     'B': [('D', 5)],
-    'C': [('D', 12)]
+    'C': [('D', 12)],
+    'D': []  # Goal node has no outgoing edges
 }
-graph1 = Graph(adjac_lis)
-graph1.a_star_algorithm('A', 'D')
+
+# Run A* algorithm
+a_star_algorithm('A', 'D', adjac_lis)
